31b0a01869322116202f96d592369a9cd8108a71
sayan <chatterjeesayan730@gmail.com>
2025-05-26 01:49:14 +0530

    backend completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import User from "../models/User.js";
 import Purchase from "../models/Purchase.js";
 import Course from "../models/Course.js";
+import CourseProgress from "../models/courseProgress.js";
 
 // get user data
 export const getUserData = async (req, res) => {
@@ -102,6 +103,120 @@ export const purchaseCourse = async (req, res) => {
     res.status(500).json({
       success: false,
       message: error.message || "Failed to purchase course",
+    });
+  }
+};
+
+// update User course progress
+export const updateUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId, lectureId } = req.body;
+    const progressData = await CourseProgress.findOne({
+      userId,
+      courseId,
+    });
+
+    if (progressData) {
+      if (progressData.lectureCompleted.includes(lectureId)) {
+        return res.status(400).json({
+          success: false,
+          message: "Lecture already completed",
+        });
+      }
+
+      progressData.lectureCompleted.push(lectureId);
+      await progressData.save();
+    } else {
+      await CourseProgress.create({
+        userId,
+        courseId,
+        lectureCompleted: [lectureId],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Course progress updated successfully",
+    });
+  } catch (error) {
+    console.error("Error updating course progress:", error);
+    res.status(500).json({
+      success: false,
+      message: error.message || "Failed to update course progress",
+    });
+  }
+};
+
+// get user course progress
+export const getUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId } = req.body;
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+
+    res.status(200).json({
+      success: true,
+      message: "Course progress fetched successfully",
+      progressData,
+    });
+  } catch (error) {
+    console.error("Error fetching course progress:", error);
+    res.status(500).json({
+      success: false,
+      message: error.message || "Failed to fetch course progress",
+    });
+  }
+};
+
+// add user rating function
+export const addUserRating = async (req, res) => {
+  const userId = req.auth.userId;
+  const { courseId, rating } = req.body;
+
+  if (!courseId || !userId || !rating || rating < 1 || rating > 5) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid rating data" });
+  }
+
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Course not found" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user || !user.enrolledCourses.includes(courseId)) {
+      return res.status(404).json({
+        success: false,
+        message: "User has not purchased this course",
+      });
+    }
+
+    const existingRatingIndex = course.courseRatings.findIndex(
+      (r) => r.userId.toString() === userId
+    );
+
+    if (existingRatingIndex > -1) {
+      course.courseRatings[existingRatingIndex].rating = rating;
+    } else {
+      course.courseRatings.push({ userId, rating });
+    }
+
+    await course.save();
+
+    res.status(200).json({
+      success: true,
+      message: "User rating added successfully",
+    });
+  } catch (error) {
+    console.error("Error adding user rating:", error);
+    res.status(500).json({
+      success: false,
+      message: error.message || "Failed to add user rating",
     });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import {
+  addUserRating,
+  getUserCourseProgress,
   getUserData,
   purchaseCourse,
+  updateUserCourseProgress,
   userEnrolledCourses,
 } from "../controllers/userController.js";
 
@@ -12,5 +15,9 @@ const userRouter = express.Router();
 userRouter.get("/data", ClerkExpressRequireAuth(), getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", ClerkExpressRequireAuth(), purchaseCourse);
+
+userRouter.post("/update-course-progress", updateUserCourseProgress);
+userRouter.post("/get-course-progress", getUserCourseProgress);
+userRouter.post("/add-rating", addUserRating);
 
 export default userRouter;
